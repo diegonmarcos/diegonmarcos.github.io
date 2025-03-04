49574af05cd1ea935e1d63cf208e3fa4e415ee92
Diego <diegonmarcos@gmail.com>
2025-03-04 11:23:15 +0100

    up

---
+++ resources/gcl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# frontpage
+git clone git@github.com:diegonmarcos/diegonmarcos.git
+# dev
+git clone git@github.com:diegonmarcos/dev.git
+# sys
+git clone git@github.com:diegonmarcos/system.git
+# graphic
+git clone git@github.com:diegonmarcos/graphic.git
+# mylib
+git clone git@github.com:diegonmarcos/mylib.git
+# front
+git clone git@github.com:diegonmarcos/front.git
+# website
+git clone git@github.com:diegonmarcos/diegonmarcos.github.io.git

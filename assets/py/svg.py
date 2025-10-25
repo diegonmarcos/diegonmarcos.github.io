5940a6a3421ccc1218a9e926ad86098d2d12fd81
diegonmarcos <diegonmarcos@gmail.com>
2025-10-25 18:43:27 +0200

    up

--- assets/py/svg.py
+++ assets/py/svg.py
@@ -1,31 +1,45 @@
 from PIL import Image
-import potrace as pypotrace
 import numpy as np
 import argparse
+from potrace import Bitmap
 
-def raster_to_svg_potrace(image_path, output_svg_path, threshold=128):
+def raster_to_svg_potrace(image_path, output_svg_path, threshold=128, invert=False):
     # 1. Load the image using Pillow
     img = Image.open(image_path).convert('L') # Convert to grayscale
 
     # 2. Convert to a 1-bit (black and white) image for Potrace
-    # Potrace works best with black shapes on a white background (or vice-versa)
-    # You might need to invert if your image has white shapes on black
-    binary_img = img.point(lambda p: 0 if p < threshold else 255, '1') # '1' for 1-bit pixels
+    binary_img = img.point(lambda p: 0 if p < threshold else 255, '1')
 
-    # 3. Use pypotrace to generate SVG
-    # Convert Pillow image to a Potrace compatible bitmap object
-    # Potrace expects 0 for black (traceable), 1 for white (background)
-    bitmap_data = np.array(binary_img).astype(bool) # Potrace takes boolean array
-    # If your image has white paperclip on white background, you might need to invert:
-    # bitmap_data = np.invert(bitmap_data)
+    # Invert if needed
+    if invert:
+        binary_img = Image.fromarray(np.invert(np.array(binary_img)))
 
-    potrace_bitmap = pypotrace.Bitmap(bitmap_data)
+    # 3. Create a Potrace bitmap
+    bm = Bitmap(binary_img)
 
     # 4. Trace the bitmap
-    path = pypotrace.trace(potrace_bitmap)
+    plist = bm.trace()
 
     # 5. Save the SVG
-    path.write_svg(output_svg_path)
+    with open(output_svg_path, "w") as fp:
+        fp.write(f'<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="{img.width}" height="{img.height}" viewBox="0 0 {img.width} {img.height}">')
+        for curve in plist:
+            fp.write('<path d="')
+            start = curve.start_point
+            fp.write(f'M{start.x},{start.y}')
+            for segment in curve:
+                if segment.is_corner:
+                    c = segment.c
+                    end = segment.end_point
+                    fp.write(f'L{c.x},{c.y}L{end.x},{end.y}')
+                else:
+                    c1 = segment.c1
+                    c2 = segment.c2
+                    end = segment.end_point
+                    fp.write(f'C{c1.x},{c1.y} {c2.x},{c2.y} {end.x},{end.y}')
+            fp.write('Z"/>')
+        fp.write('</svg>')
+
     print(f"SVG saved to {output_svg_path}")
 
 if __name__ == "__main__":
@@ -33,6 +47,7 @@ if __name__ == "__main__":
     parser.add_argument('input_file', help='The input raster image file (e.g., PNG, JPG).')
     parser.add_argument('output_file', help='The output SVG file.')
     parser.add_argument('--threshold', type=int, default=128, help='The threshold for converting to a binary image (0-255).')
+    parser.add_argument('--invert', action='store_true', help='Invert the colors of the image before tracing.')
     args = parser.parse_args()
 
-    raster_to_svg_potrace(args.input_file, args.output_file, args.threshold)
+    raster_to_svg_potrace(args.input_file, args.output_file, args.threshold, args.invert)

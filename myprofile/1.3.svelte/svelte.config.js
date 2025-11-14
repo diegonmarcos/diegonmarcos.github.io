4ed142e2a28e514ef2c27c0941f58cd234176174
diegonmarcos <diegonmarcos@gmail.com>
2025-11-14 19:14:31 +0100

    up

--- myprofile/1.3.svelte/svelte.config.js
+++ myprofile/1.3.svelte/svelte.config.js
@@ -7,8 +7,8 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			pages: '../build',
-			assets: '../build',
+			pages: 'build',
+			assets: 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true

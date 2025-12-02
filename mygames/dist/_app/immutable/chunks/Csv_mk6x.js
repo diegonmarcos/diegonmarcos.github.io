3c489cc3fbe56b0f65a58c76f9dc148aa6783538
diegonmarcos <diegonmarcos@gmail.com>
2025-12-02 21:16:04 +0100

    up

--- mygames/dist/_app/immutable/chunks/Csv_mk6x.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_sdwqnr?.base??"/mygames",a=globalThis.__sveltekit_sdwqnr?.assets??s??"";export{a,s as b};
---
+++ mygames/dist/_app/immutable/chunks/DJB51C5g.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1c1kpy6?.base??"/mygames",a=globalThis.__sveltekit_1c1kpy6?.assets??s??"";export{a,s as b};
